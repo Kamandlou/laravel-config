3804e9de03ff38bea92a852fc53243766458c83f
Kamandlou <ahkamandlou@gmail.com>
2022-08-22 15:58:24 +0430

    update Config.php

--- src/Models/Config.php
+++ src/Models/Config.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
-    protected string $table = 'configs';
+    protected $guarded = ['id'];
+    protected $table = 'configs';
+
+    public static function getConfig(string $key): array
+    {
+        return self::where('key', $key)->pluck('value')->toArray();
+    }
+
+    public static function firstConfig(string $key): string
+    {
+        return self::where('key', $key)->first('value')->value;
+    }
 }
